8325b56517980f87831b2b0b151241506ce48926
arpegx <arpegx@posteo.de>
2025-02-12 20:23:23 +0100

    BCP-2-init: raw procedure

--- app/Command/Init.php
+++ app/Command/Init.php
@@ -7,5 +7,22 @@ class Init
     public static function handle(array $argv)
     {
         print __METHOD__ . PHP_EOL;
+
+        // variables
+        $config_dir = getenv("HOME") . "/.config/bacup";
+        $config = $config_dir . "/config.xml";
+        $default = file_get_contents("data/default.xml");
+
+        // repeated call
+        if (file_exists($config)) {
+            print "Configuration exists already\n";
+            return 0;
+        }
+
+        // setup configuration
+        mkdir($config_dir, 0700, true);
+        file_put_contents($config, $default);
+
+        print "Configuration established\n";
     }
 }
