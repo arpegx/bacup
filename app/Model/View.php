6cb2c6ac42129397b4cefe45506cf71fe381d9ad
arpegx <arpegx@posteo.de>
2025-03-09 00:06:52 +0100

    BCP-7-help: multiple templates

--- app/Model/View.php
+++ app/Model/View.php
@@ -11,40 +11,26 @@ class View
 
         //. source view
         $file = realpath(self::$views . $view . ".html");
-        $content = file_get_contents($file);
-        $output = $content;
+        $output = file_get_contents($file);
 
         //. templates
-        // $banner = file_get_contents(realpath(self::$views . "banner.html"));
-        // return "<div>" . $banner . $output . "</div>";
-
         if (str_contains($output, "@template")) {
-            // return "true";
-            $starts_at = strpos($output, "@template(\"");
-            $ends_at = strpos(substr($output, $starts_at), "\")");
 
-            $template_str = substr($output, $starts_at, $ends_at - $starts_at + 10) . PHP_EOL;
-            $template = substr($output, $starts_at + 11, $ends_at - 3 - $starts_at);
+            preg_match_all('/@template\("[a-z]+"\)/', $output, $matches);
 
-            $template_replacement = file_get_contents(self::$views . $template . ".html");
-
-            // echo "Variables \n";
-            // var_dump(
-            //     [
-            //         $template_str,
-            //         $template_replacement,
-            //     ]
-            // );
-
-            $output = str_replace(
-                $template_str,
-                $template_replacement,
-                $output
+            $templates = array_map(
+                fn($match) => sscanf($match, '@template(" %[a-z]')[0],
+                $matches[0]
             );
 
-            // echo "replaced \n";
-            // var_dump($output);
-            // echo "\n";
+            array_walk($templates, function (&$template) use (&$output) {
+
+                $output = str_replace(
+                    "@template(\"" . $template . "\")",
+                    file_get_contents(self::$views . $template . ".html"),
+                    $output
+                );
+            });
         }
 
         //. dynamic content
