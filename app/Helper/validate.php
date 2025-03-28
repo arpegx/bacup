c4d8710b7a4e35850ff6c2644317e6a5451f03a0
arpegx <arpegx@posteo.de>
2025-03-28 15:14:02 +0100

    BCP-4-track: namespacing helper fn

--- app/Helper/validate.php
+++ app/Helper/validate.php
@@ -7,17 +7,20 @@ namespace Arpegx\Bacup\Helper;
 use Arpegx\Bacup\Routing\Rules;
 use Webmozart\Assert\Assert;
 
-function validate(array $data, array $rules)
-{
-    foreach ($rules as $rule_key => $rule_values) {
-        for ($i = 0; $i < sizeof($rule_values); $i++) {
-            $result = array();
-            if ($rule_values[$i] == Rules::REQUIRED) {
-                $result = call_user_func([Rules::class, $rule_values[$i]], $rule_key, $data);
-            } else {
-                $result = call_user_func([Rules::class, $rule_values[$i]], $data[$rule_key]);
+if (!function_exists(__NAMESPACE__ . '\validate')) {
+    function validate(array $data, array $rules)
+    {
+        //do refactor
+        foreach ($rules as $rule_key => $rule_values) {
+            for ($i = 0; $i < sizeof($rule_values); $i++) {
+                $result = array();
+                if ($rule_values[$i] == Rules::REQUIRED) {
+                    $result = call_user_func([Rules::class, $rule_values[$i]], $rule_key, $data);
+                } else {
+                    $result = call_user_func([Rules::class, $rule_values[$i]], $data[$rule_key]);
+                }
+                Assert::true($result["result"], $result["message"]);
             }
-            Assert::true($result["result"], $result["message"]);
         }
     }
 }
