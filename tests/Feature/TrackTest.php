065679a28a3322b44f724232cb0f2ce2d011b397
arpegx <arpegx@posteo.de>
2025-03-30 04:18:19 +0200

    BCP-4-track: tests

--- tests/Feature/TrackTest.php
+++ tests/Feature/TrackTest.php
@@ -2,6 +2,49 @@
 
 declare(strict_types=1);
 
-test("Architecture: Track::handle fn exists", function () {
-    expect('Arpegx\Bacup\Command\Track')->toHaveMethod('handle');
+use Arpegx\Bacup\Command\Track;
+
+describe("Track", function () {
+
+    //. handle ------------------------------------------------------------------------------------
+    describe("handle", function () {
+        test("Architecture: Track::handle fn exists", function () {
+            expect('Arpegx\Bacup\Command\Track')->toHaveMethod('handle');
+        });
+
+        test('Validaton', function () {
+            // stdout isnt quiet catchable, therefore this semi-finished solution
+            exec("./bacup track target=app", $output, $result);
+            expect(implode($output))->toContain("/usr/src/bacup/app added");
+        });
+    });
+
+    //. resolve -----------------------------------------------------------------------------------
+    describe("resolve", function () {
+        test("Validation", function () {
+
+            expect(
+                reflect(
+                    Track::class,
+                    invoke: ["resolve", [["target=app"]]]
+                )
+            )->toEqual(["confirm" => "true", "target" => "/usr/src/bacup/app"]);
+
+            expect(
+                fn() =>
+                reflect(
+                    Track::class,
+                    invoke: ["resolve", [["target=app="]]]
+                )
+            )->toThrow(\Webmozart\Assert\InvalidArgumentException::class);
+
+            expect(
+                fn() =>
+                reflect(
+                    Track::class,
+                    invoke: ["resolve", [["undefined=app"]]]
+                )
+            )->toThrow(\Exception::class);
+        });
+    });
 });
